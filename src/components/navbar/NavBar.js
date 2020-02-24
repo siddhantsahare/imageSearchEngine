3dc28a2314519472cdcc6f81df9f1e3ff1b1843e
siddhants8019 <siddhants8019@gmail.com>
2020-02-24 12:58:08 +0530

    developement phase complete

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => (
+  <AppBar style={{ background: 'red' }} title="Search For Images in Volumes" />
+);
+
+export default NavBar;
